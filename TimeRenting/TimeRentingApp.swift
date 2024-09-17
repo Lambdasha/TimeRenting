b70e1f4577e22341d0a7740ded83e3de75ccc92b
Lambdasha <shajingshu@gmail.com>
2024-09-17 14:31:25 -0700

    Initial Commit

---
+++ TimeRenting/TimeRentingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeRentingApp.swift
+//  TimeRenting
+//
+//  Created by Echo Targaryen on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeRentingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeRentingTests/TimeRentingTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeRentingTests.swift
+//  TimeRentingTests
+//
+//  Created by Echo Targaryen on 9/17/24.
+//
+
+import Testing
+@testable import TimeRenting
+
+struct TimeRentingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
